72c804e27890099105fcb33b9a3aec09ac2644e1
Henrique dos Santos Alves <henriquelp321@gmail.com>
2023-10-04 16:16:56 -0300

    código do trabalho

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # AprendizagemDeMaquina-Titanic
+Trabalho de Aprendizagem de máquina
+
+Alunos:
+Henrique dos Santos Alves(2124290023)
+Stefany Ferreira da Silva(2214290056)
---
+++ Trabalho.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Lendo dados do arquivo 'dados4.csv'
+df = pd.read_csv('dados/dados4.csv')
+
+# # Criando variáveis para idade
+ageColumn = df["age"]
+ageMode = ageColumn.mode()[0]
+
+# Preenche os valores nulos na coluna 'ageColumn'
+ageColumn.fillna(ageMode , inplace=True)
+df["age"] = ageColumn.astype(int)
+
+#################
+# Exercício nº1 #
+#################
+
+
+
+# criação do arquivo e escrita dos dados
+
+# criando o arquivo de resposta
+arquivo = open("Resposta01.txt","w")
+
+# criando as colunas com os nomes, idades e sexo
+arquivo.write(str(df[["name","sex","age"]].to_string()))
+arquivo.close()
+
+####################################################################################################
+
+#################
+# Exercício nº2 #
+#################
+
+# variáveis para o sexo
+sexColumn = df["sex"]
+sexMode = sexColumn.mode()[0]
+
+# Preenchimentos de campos nulos na coluna 'sexCollumn'
+sexColumn.fillna(sexMode, inplace=True)
+
+# Variáveis para contagem de cada sexo
+maleCount = 0
+femaleCount = 0
+
+# for para a contagem dos sexos
+for item in sexColumn:
+    if(item == "male"):
+        maleCount += 1
+    elif( item == "female"):
+        femaleCount += 1
+
+# Retorna no terminal a quatidade de homens e mulheres embarcados
+print("\nNo barco haviam "+str(maleCount)+" homens e haviam "+str(femaleCount)+" mulheres\n")
+
+# ##################################################################################################
+
+# #################
+# # Exercício nº3 #
+# #################
+
+# variáveil de sobrevivente
+survivedColumn = df["survived"]
+survivedMode = survivedColumn.mode()[0]
+
+# Preenchimento de campos nulos na coluna 'survivedColumn'
+survivedColumn.fillna(survivedMode , inplace=True)
+
+# Variáveis de contagem de mortos e sobreviventes
+survivedCount = 0
+deadCount = 0
+
+# for para a contagem de mortos e sobreviventes
+for item in survivedColumn:
+    if(item == 0):
+        deadCount += 1
+    elif( item == 1):
+        survivedCount += 1
+
+# Utilizando um array para guardar os dados para o gráfico(pizza)
+dados = [deadCount,survivedCount]
+
+# gráfico(pizza) com porcentagem de sobreviventes e não sobreviventes
+plt.pie(dados, labels = ['Não Sobreviventes','Sobreviventes'], autopct='%1.0f%%')
+plt.show()
+
+# ##################################################################################################
+
+# #################
+# # Exercício nº4 #
+# #################
+
+# Variáveis para a coluna tarifa
+fareColumn = df["fare"]
+fareMode = fareColumn.mode()[0]
+
+# preenchendo os campos nulos da coluna tarifa
+fareColumn.fillna(fareMode, inplace=True)
+
+# grafico de disperção da idade
+plt.scatter ( ageColumn, fareColumn, alpha=0.5 )
+plt.title("idade por tarifa")
+plt.xlabel("idade")
+plt.ylabel("tarifa")
+plt.show()
