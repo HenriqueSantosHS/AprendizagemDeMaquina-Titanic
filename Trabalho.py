3b8444ead566d3b1d8a3c3aed6cc91ad1c254ac1
Henrique dos Santos Alves <henriquelp321@gmail.com>
2023-10-04 16:26:48 -0300

    Término do código

--- Trabalho.py
+++ Trabalho.py
@@ -31,26 +31,26 @@ arquivo.close()
 # Exercício nº2 #
 #################
 
-# Variáveis para o sexo
-sexColumn = df["sex"]
-sexMode = sexColumn.mode()[0]
+# # Variáveis para o sexo
+# sexColumn = df["sex"]
+# sexMode = sexColumn.mode()[0]
 
-# Preenchimentos de campos nulos na coluna 'sexCollumn'
-sexColumn.fillna(sexMode, inplace=True)
+# # Preenchimentos de campos nulos na coluna 'sexCollumn'
+# sexColumn.fillna(sexMode, inplace=True)
 
-# Variáveis para contagem de cada sexo
-maleCount = 0
-femaleCount = 0
+# # Variáveis para contagem de cada sexo
+# maleCount = 0
+# femaleCount = 0
 
-# for para a contagem dos sexos
-for item in sexColumn:
-    if(item == "male"):
-        maleCount += 1
-    elif( item == "female"):
-        femaleCount += 1
+# # for para a contagem dos sexos
+# for item in sexColumn:
+#     if(item == "male"):
+#         maleCount += 1
+#     elif( item == "female"):
+#         femaleCount += 1
 
-# Retorna no terminal a quatidade de homens e mulheres embarcados
-print("\nNo barco haviam "+str(maleCount)+" homens e haviam "+str(femaleCount)+" mulheres\n")
+# # Retorna no terminal a quatidade de homens e mulheres embarcados
+# print("\nNo barco haviam "+str(maleCount)+" homens e haviam "+str(femaleCount)+" mulheres\n")
 
 # ##################################################################################################
 
@@ -58,30 +58,30 @@ print("\nNo barco haviam "+str(maleCount)+" homens e haviam "+str(femaleCount)+"
 # # Exercício nº3 #
 # #################
 
-# Variáveil de sobrevivente
-survivedColumn = df["survived"]
-survivedMode = survivedColumn.mode()[0]
+# # Variáveil de sobrevivente
+# survivedColumn = df["survived"]
+# survivedMode = survivedColumn.mode()[0]
 
-# Preenchimento de campos nulos na coluna 'survivedColumn'
-survivedColumn.fillna(survivedMode , inplace=True)
+# # Preenchimento de campos nulos na coluna 'survivedColumn'
+# survivedColumn.fillna(survivedMode , inplace=True)
 
-# Variáveis de contagem de mortos e sobreviventes
-survivedCount = 0
-deadCount = 0
+# # Variáveis de contagem de mortos e sobreviventes
+# survivedCount = 0
+# deadCount = 0
 
-# for para a contagem de mortos e sobreviventes
-for item in survivedColumn:
-    if(item == 0):
-        deadCount += 1
-    elif( item == 1):
-        survivedCount += 1
+# # for para a contagem de mortos e sobreviventes
+# for item in survivedColumn:
+#     if(item == 0):
+#         deadCount += 1
+#     elif( item == 1):
+#         survivedCount += 1
 
-# Utilizando um array para guardar os dados para o gráfico(pizza)
-dados = [deadCount,survivedCount]
+# # Utilizando um array para guardar os dados para o gráfico(pizza)
+# dados = [deadCount,survivedCount]
 
-# Gráfico(pizza) com porcentagem de sobreviventes e não sobreviventes
-plt.pie(dados, labels = ['Não Sobreviventes','Sobreviventes'], autopct='%1.0f%%')
-plt.show()
+# # Gráfico(pizza) com porcentagem de sobreviventes e não sobreviventes
+# plt.pie(dados, labels = ['Não Sobreviventes','Sobreviventes'], autopct='%1.0f%%')
+# plt.show()
 
 # ##################################################################################################
 
@@ -89,16 +89,22 @@ plt.show()
 # # Exercício nº4 #
 # #################
 
-# Variáveis para a coluna tarifa
-fareColumn = df["fare"]
-fareMode = fareColumn.mode()[0]
+# # Variáveis para a coluna tarifa
+# fareColumn = df["fare"]
+# fareMode = fareColumn.mode()[0]
 
-# Preenchendo os campos nulos da coluna tarifa
-fareColumn.fillna(fareMode, inplace=True)
+# # Preenchendo os campos nulos da coluna tarifa
+# fareColumn.fillna(fareMode, inplace=True)
 
-# Grafico de disperção da idade
-plt.scatter ( ageColumn, fareColumn, alpha=0.5 )
-plt.title("idade por tarifa")
-plt.xlabel("idade")
-plt.ylabel("tarifa")
-plt.show()
+# # Grafico de disperção da idade
+# plt.scatter ( ageColumn, fareColumn, alpha=0.5 )
+
+# # Titulo do gráfico
+# plt.title("idade por tarifa")
+
+# # Nome 'idade' no eixo 'X'
+# plt.xlabel("idade")
+
+# # Nome 'tarifa' no eixo 'Y'
+# plt.ylabel("tarifa")
+# plt.show()
